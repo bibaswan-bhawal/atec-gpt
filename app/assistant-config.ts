1727d0f0811950ce6ce4aadb28e3c71717c5b56f
Bibaswan Bhawal <bibaswan3600@gmail.com>
2024-11-16 14:37:15 -0800

    wip

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,5 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = ''; // set your assistant ID here
 
-if (assistantId === "") {
-  assistantId = process.env.OPENAI_ASSISTANT_ID;
+if (assistantId === '') {
+	assistantId = process.env.OPENAI_ASSISTANT_ID || '';
 }
